3d3b45611086f847c0f0da14944becbbdf3f5e47
LitherinStyke <li_styke@outlook.com>
2025-04-22 13:15:24 -0700

    Refactor with stats script and updated gitnore

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
+from stats import get_words_from_book
+
 def get_book_text(path_to_file:str):
     with open(path_to_file) as f:
         file_contents = f.read()
         return file_contents
 
-def get_words_from_book(path_to_file:str):
-    book_contents = get_book_text(path_to_file)
-    words = book_contents.split()
-    return words
-
 def main():
-    print(f'{len(get_words_from_book('books/frankenstein.txt'))} words found in the document')
+    print(f'{get_words_from_book(get_book_text('books/frankenstein.txt'))} words found in the document')
 
 main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_words_from_book(book_contents:str):
+    words = book_contents.split()
+    return len(words)
